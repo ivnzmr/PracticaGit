611c27eed21d9dffc5b9b670b4150c11e82c45f5
ivnzmr <ivnzmr99@gmail.com>
2026-01-28 19:54:51 +0100

    Revert "Ejercicio 5"
    This reverts commit c296124a9fb951ec33106d3474a8178b47846d4d.

--- Calculadora.java
+++ Calculadora.java
@@ -3,12 +3,12 @@ package Calculadora;
 public class Calculadora {
    
     public int sumar(int a, int b) {
-        return a * b;
+        return a + b;
     }
     
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
         int resultado = calc.sumar(5, 3);
-        System.out.println("5 * 3 = " + resultado);
+        System.out.println("5 + 3 = " + resultado);
     }
 }
